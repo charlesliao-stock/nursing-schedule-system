d84bf50a49d491b3057bbf7f703bbaa1585850e2
charlesliao-stock <charlesliao.tw@gmail.com>
2025-12-13 01:31:44 +0800

    Update UserService.js

--- js/services/firebase/UserService.js
+++ js/services/firebase/UserService.js
@@ -1,4 +1,4 @@
-import { db, auth } from "../../../config/firebase.config.js";
+import { db, auth } from "../../config/firebase.config.js";
 import { 
     collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc, 
     query, where, serverTimestamp 
@@ -74,4 +74,5 @@ export const userService = {
         );
     }
 };
+
 
