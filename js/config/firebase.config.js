66fd9616fce26aa0b79c0e62d7e9e7aa2bf0cd14
charlesliao-stock <charlesliao.tw@gmail.com>
2025-12-13 01:53:21 +0800

    Update firebase.config.js

--- js/config/firebase.config.js
+++ js/config/firebase.config.js
@@ -1,6 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// js/config/firebase.config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA2B_rDKi7JyLaYpJd-lfFNXZ1BJUzpu-k",
@@ -11,12 +12,15 @@ const firebaseConfig = {
     appId: "1:561144664580:web:3d4397a5cbd7f788b1db51",
     measurementId: "G-V0DBP9RZ7P"
 };
-// 初始化 Firebase
+// 初始化 App
 const app = initializeApp(firebaseConfig);
 
-// 初始化服務
+// 初始化並匯出 db 與 auth
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-// 匯出 db 和 auth 供其他檔案使用
+// 加入這行來除錯，按 F12 應該要看到這個物件不是 undefined
+console.log("Firebase Config Loaded, DB:", db); 
+
 export { db, auth };
+
